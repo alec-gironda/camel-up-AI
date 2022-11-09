83ae9a42c4e5a453f686cb91f983b4a7bff8a93b
Alec Gironda <agironda@middlebury.edu>
2022-11-09 15:54:33 -0500

    work on expand

--- tree.py
+++ tree.py
@@ -2,11 +2,12 @@ from collections import deque
 
 class GameStateNode:
 
-    def __init__(self, board_state,dice_left,bets_left):
+    def __init__(self,board_state,dice_left,bets_left,money):
         self.board_state = board_state
         self.dice_left = dice_left
         self.bets_left = bets_left
-        self.bets_made =
+        self.bets_made = bets_made
+        self.money = money
 
 
     def is_complete(self):
@@ -17,6 +18,12 @@ class GameStateNode:
 
     def expand(self):
 
+        #bet
+
+
+        #roll
+        
+
 
         return children
 
@@ -25,12 +32,16 @@ class GameStateNode:
 
 if __name__ == "__main__":
 
+    board_state = {i+1:tuple(0 for i in range(25)) for i in range(17)}
     bets_left = {1:(5,3,2),2:(5,3,2),3:(5,3,2),4:(5,3,2),5:(5,3,2)}
     bets_made = {}
+    dice_left = set([1,2,3,4,5])
+    money = 0
+    print(board_state)
 
-    curr = GameStateNode({1:(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),2:(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)})
+    # curr = GameStateNode({1:(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),2:(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)})
 
-    print(curr.game_state)
+    # print(curr.game_state)
 
 # B G O W R
 # 0 0 0 0 0
